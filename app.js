750d6e9fa17c0959f5f876ae820d80b2a4f8d49e
Mzee Sotsaka <mzwandile@bbd.co.za>
2020-06-26 23:23:22 +0200

    added basic server and app connection

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
-const http = require('http');
-
+//***************************** Configuraion for node
 const hostname = '127.0.0.1';
+
+const express = require('express');
+var bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+app.use(cors())
+app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+app.use(bodyParser.json());
+app.listen(port, hostname, () => {
+    console.log('Listening on port', port)
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.post('/users/signin', function (req, res) {
+    console.log(req.body);
+    res.writeHead(200);
+    console.log('respose recieved and sent back');
+})
+
