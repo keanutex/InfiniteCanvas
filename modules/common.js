a3d8abd55b4ada55328f68682a6d331c17b6a31e
Keanu Teixeira <keanutex@gmail.com>
2020-07-09 17:15:44 +0200

    Took out some nonsense logging

--- modules/common.js
+++ modules/common.js
@@ -3,12 +3,10 @@ const { Int } = require('mssql/msnodesqlv8');
 
 async function isBlocked(userId) {
     const sql = `SELECT * FROM status INNER JOIN users ON status."statusId" = users."statusId" WHERE users."userId" = @userId`;
-    console.log("OVER HERHER")
     try {
         const pool = await poolPromise;
 
         const response = await pool.request().input('userId', Int, userId).query(sql)
-        console.log("HEREHEHEHEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE", response)
         if (response.recordset[0].name == "Blocked") {
             return true;
         } else {
